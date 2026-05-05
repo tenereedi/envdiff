e11fe4759e01b0f9f0b13f280761108346b08c3a
Tene Reedi <tenereedi@users.noreply.github.com>
2026-05-06 04:33:07 +0800

    feat: add get_version() helper function to public API

--- envdiff/__init__.py
+++ envdiff/__init__.py
@@ -5,9 +5,28 @@ __author__ = "envdiff contributors"
 
 from envdiff.parser import parse_env_file, parse_env_string, EnvEntry, ParseResult
 
+
+def get_version() -> str:
+    """Return the current version of envdiff.
+
+    Returns
+    -------
+    str
+        The version string in ``MAJOR.MINOR.PATCH`` format.
+
+    Examples
+    --------
+    >>> import envdiff
+    >>> envdiff.get_version()
+    '0.1.0'
+    """
+    return __version__
+
+
 __all__ = [
     "parse_env_file",
     "parse_env_string",
     "EnvEntry",
     "ParseResult",
+    "get_version",
 ]
